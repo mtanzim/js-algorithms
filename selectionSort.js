54be644528a44cef777b2410ae4adf35b17ec443
Tanzim Mokammel <mtanzim@gmail.com>
2018-08-23 13:57:49 +0800

    finished selection sort

--- selectionSort.js
+++ selectionSort.js
@@ -1,19 +1,25 @@
-const unsortedArr = [12, 11, 9, 5, 4, 2, 1];
+const unsortedArr = [-12, 11, 9, -5, 4, 2, 1];
 
 function selectionSort(origArr, type = 'selection') {
 
   console.log(`Applying ${type} sort`);
 
   let arr = [].concat(origArr);
-/*   for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+  for (let i = 0; i < arr.length; i++) {
+    let index = i;
+    let minVal = arr[i];
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j]<minVal) {
+        minVal=arr[j];
+        index=j;
       }
     }
-  } */
+    console.log(`\nPass: ${i}`);
+    console.log(`minval: ${minVal}`);
+    console.log(`arr: ${arr}\n`);
+    arr[index] = arr[i];
+    arr[i] = minVal;
+  }
 
   return arr;
 }
