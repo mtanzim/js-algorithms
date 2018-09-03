ff2e06c784d9bf3f81ad904799fdccb61380dfaa
Tanzim Mokammel <mtanzim@gmail.com>
2018-09-04 00:10:03 +0900

    finished tower of hanoi!

--- recursion/towerOfHanoi.js
+++ recursion/towerOfHanoi.js
@@ -1,42 +1,65 @@
-let A = {name: 'A', data: []};
-let B = {name: 'B', data: []};
-let C = {name: 'C', data: []};
+// use this variable to control how many pegs are to be used
+// globalN is the disk with the largest radius
+// Objective is to move blocks from A to C
+let globalN = 3;
+
+let A = { name: 'A', data: [] };
+let B = { name: 'B', data: [] };
+let C = { name: 'C', data: [] };
+
+let globalCounter = 0;
+
+function checkSorted(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < arr[i + 1]) throw new Error('Dafuq check your code!');
+  }
+}
 
 function move(n, towerFrom, towerTo, towerIntermediate) {
-  
 
-/*   console.log(`A:[${A}]`);
-  console.log(`B:[${B}]`);
-  console.log(`C:[${C}]`); */
+  // console.log(`Need to move ${n} blocks from tower ${towerFrom.name} to ${towerTo.name} using ${towerIntermediate.name}`);
 
-  
+  checkSorted(towerFrom.data);
+  checkSorted(towerTo.data);
+  checkSorted(towerIntermediate.data);
 
-  if (n == 2) {
+  if (n <= 2) {
     towerIntermediate.data.push(towerFrom.data.pop());
+    globalCounter++;
     towerTo.data.push(towerFrom.data.pop());
+    globalCounter++;
     towerTo.data.push(towerIntermediate.data.pop());
+    globalCounter++;
   } else {
-    move(n-1,towerFrom, towerIntermediate, towerTo);
+    move(n - 1, towerFrom, towerIntermediate, towerTo);
     towerTo.data.push(towerFrom.data.pop());
+    globalCounter++;
+    move(n - 1, towerIntermediate, towerTo, towerFrom);
   }
 
-  console.log(`Moving ${n} blocks from tower ${towerFrom.name} to ${towerTo.name} using ${towerIntermediate.name}`);
+  console.log(`\nMoved ${n} blocks from tower ${towerFrom.name} to ${towerTo.name} using ${towerIntermediate.name}`);
   console.log(`End of N: ${n}`);
   console.log(`A:[${A.data}]`);
   console.log(`B:[${B.data}]`);
   console.log(`C:[${C.data}]`);
-
 }
 
-// n is the disk with the largest radius
-// Objective is to move blocks from A to C
-A.data = [4, 3,2,1];
-globalN = A.data.length;
 
-console.log(`START:`);
+
+// set up 'from' array 
+for (let j = globalN; j > 0; j--) A.data.push(j);
+
+console.log(`\nSTART:`);
 console.log(`A:[${A.data}]`);
 console.log(`B:[${B.data}]`);
 console.log(`C:[${C.data}]`);
-move(globalN,A,C,B)
-// move A to C using ToH rules
+
+if (globalN <=2) throw new Error ('Please set number of pegs higher than 2!');
+move(globalN, A, C, B);
+
+console.log(`\nEND:`);
+console.log(`A:[${A.data}]`);
+console.log(`B:[${B.data}]`);
+console.log(`C:[${C.data}]`);
+console.log(`\nUsed ${globalCounter} moves.`);
 
