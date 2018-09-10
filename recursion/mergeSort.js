91ef572b07c834c8da5d9162e6f26c3f0a7a01ba
Tanzim Mokammel <mtanzim@gmail.com>
2018-09-10 20:05:06 +0900

    merge sort complete

--- recursion/mergeSort.js
+++ recursion/mergeSort.js
@@ -1,83 +1,75 @@
-// const unsortedArr = [12, 11, 9, 5, 4, 2, 1];
 const testSorted = require('../basicSorting/testSorted');
 const unsortedArrs = require('../basicSorting/arrCollection');
 
+const merge = function (arr, start, middle, end, debug=false) {
 
-const merge = function (arr, start, middle, end) {
-  // console.log('Doing nothing for now!');
-  // let tempArrFull = arr.slice(start, end + 1);
-
-  let n1 = middle - start + 1;
-  let n2 = end - middle;
-
+  // let n1 = middle - start + 1;
+  // let n2 = end - middle;
 
   // copy to temp arrays
   let left = arr.slice(start, middle + 1);
   let right = arr.slice(middle + 1, end + 1);
-  // console.log(tempArrFull);
-  console.log(`\narr : [${arr}]`);
-  console.log(`start : ${start}`);
-  console.log(`middle : ${middle}`);
-  console.log(`end : ${end}`);
-  console.log(`left : [${left}]`);
-  console.log(`right : [${right}]`);
+
+  if (debug) {
+    console.log(`\narr : [${arr}]`);
+    console.log(`start : ${start}`);
+    console.log(`middle : ${middle}`);
+    console.log(`end : ${end}`);
+    console.log(`left : [${left}]`);
+    console.log(`right : [${right}]`);
+  }
+
+  // if (left.length !== n1 || right.length !== n2) throw new Error('Not correct!!!!');
 
   let i = 0;
   let j = 0;
+  let k = start;
 
 
+  while (i < left.length && j < right.length) {
 
-
-  for (let k = start; k <= end && i < left.length && j < right.length; k ++) {
-
-    if (left.length === 1 && right.length === 1) {
-      if (left[i] <= right[j]) {
-        arr[k] = left[i];
-        arr[k+1] = right[j];
-      } else {
-        arr[k] = right[j];
-        arr[k+1] = left[i];
-      }
-      break;
-    }
-    
     if (left[i] <= right[j]) {
-      arr[k] =  left[i];
-      i ++;
+      arr[k] = left[i];
+      i++;
     } else {
-      arr[k] = right [j];
-      j ++;
+      arr[k] = right[j];
+      j++;
     }
-  }
+    k++;
+  } 
 
-  console.log(`merged : [${arr}]`);
+  // copy over remainder of left if any 
+  while (i < left.length) {
+    arr[k] = left[i];
+    k ++;
+    i ++;
+  };
+  // copy over remainder of right if any 
+  while (j < right.length) {
+    arr[k] = right[j];
+    k ++;
+    j ++;
+  };
+
+  // console.log(`merged : [${arr}]`);
 
 }
 
 const mergeSort = function (origArr, start, end, type = 'merge') {
-
-
   let middle = Math.floor((start + end) / 2);
-
   if (start < end) {
-    // console.log(`Applying ${type} sort with start: ${start}, end: ${end}, middle: ${middle}`);
     mergeSort(origArr, start, middle);
     mergeSort(origArr, middle + 1, end);
-    merge(origArr, start, middle, end);
-    // console.log(`Applied ${type} sort with start: ${start}, end: ${end}, middle: ${middle}`);
+    merge(origArr, start, middle, end, false);
   }
-
-
-  return [0, 0];
-
-  // return (testSorted(arr) ? arr : new Error('Not sorted!'));
+  return origArr;
 }
 // use when running code by
-unsortedArrs.filter((d, i) => i === 1).forEach(unsortedArr => {
+unsortedArrs.forEach(unsortedArr => {
   console.log(`\nInitial array is ${unsortedArr}`);
   let sortedArr = [].concat(mergeSort(unsortedArr, 0, unsortedArr.length - 1));
-  console.log(`sorted array is ${sortedArr}`);
-  // console.log(`confirm sorted ${testSorted(sortedArr)}\n`);
+  console.log(`sorted array is [${sortedArr}]`);
+  console.log(`confirm sorted ${testSorted(sortedArr)}\n`);
 });
 
 module.exports = mergeSort;
