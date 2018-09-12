78bf5258f9fa4bb8b6b2b7619bf4ac34970bf7f3
Tanzim Mokammel <mtanzim@gmail.com>
2018-09-12 19:59:49 +0900

    refactored delete code

--- binarySearchTrees/BST.js
+++ binarySearchTrees/BST.js
@@ -5,18 +5,19 @@ class BST {
     this.root = null;
   }
   printBST() {
-    console.log(JSON.stringify(this, null, 2));
+    // console.log(this);
+    console.log( this && JSON.stringify(this, null, 2));
   }
 
   findNode(input) {
     return this.root.findNode(input, this.root) || null;
   }
 
-  findMin () {
+  findMin() {
     return this.root.findMinNode(this.root);
   }
 
-  findMax () {
+  findMax() {
     return this.root.findMaxNode(this.root);
   }
 
@@ -39,46 +40,21 @@ module.exports = BST;
 
 const driverCode = () => {
 
-  // let leftNodeA = new TreeNode(25);
-  // let rightNodeA = new TreeNode(39);
-  // let leftParentNode = new TreeNode(33, leftNodeA, rightNodeA);
-
-  // let leftNodeB = new TreeNode(60);
-  // let rightNodeB = new TreeNode(78);
-  // let rightParentNode = new TreeNode(65, leftNodeB, rightNodeB);
-
-  // let grandParentNode = new TreeNode(0, leftParentNode, rightParentNode);
-
-  // grandParentNode.printNode();
-
-  // myBST = new BST(grandParentNode);
-
-  let treeArr = [2, 4, 3, 5, -1];
+  let treeArr = [52, 33, 25, 12, 27, 39, 34, 48, 65, 60, 78, 72, 90];
 
   myBST = new BST();
   // console.log(`\n\n${myBST.findNode(39)}`);
 
   treeArr.forEach(a => myBST.insertNode(a));
   myBST.printBST();
-  // console.log(myBST.findMin().node.data);
-  // console.log(myBST.findMax().node.data);
-  /*   console.log('\nparents:')
-  treeArr.forEach(a => console.log(`${myBST.findNode(a).parent ? myBST.findNode(a).parent.data : 'no parent'}`));
-  console.log('\nnodes:')
-  treeArr.forEach(a => console.log(`${myBST.findNode(a).node.data}`));
-  console.log(`\nisLeftChild`)
-  treeArr.forEach(a => console.log(`${myBST.findNode(a).isLeftChild}`)); */
-  
-  // delete each node
+
+  // delete a node
   treeArr
-  .filter(d => d === -1)
-  .forEach ( d => myBST.deleteNode(d));
-  
-  myBST.printBST();
-  // console.log(myBST.findMin().node.data);
-  // console.log(myBST.findMax().node.data);
+    // .filter(d => d === -1)
+    .forEach(d => myBST.deleteNode(d));
+    
+    // myBST.printBST();
+
 }
 
 driverCode();
-
-// leftNodeA.printNode();
--- binarySearchTrees/TreeNode.js
+++ binarySearchTrees/TreeNode.js
@@ -5,10 +5,17 @@ class TreeNode {
     this.rightChild = right;
   }
 
-  updateRoot (node) {
-    this.data = node.data;
-    this.leftChild = node.leftChild;
-    this.rightChild = node.rightChild;
+  updateRoot(node) {
+    if (node){
+      this.data = node.data;
+      this.leftChild = node.leftChild ;
+      this.rightChild = node.rightChild;
+    } else {
+      console.log('Nulling tree!');
+      this.data = null;
+      this.leftChild = null ;
+      this.rightChild = null;
+    }
   }
 
   findNode(input, curNode, parent = null, isLeftChild = false) {
@@ -43,17 +50,17 @@ class TreeNode {
   deleteNode(input, startNode) {
     let nodeToDel = this.findNode(input, startNode);
     if (nodeToDel.node) {
+      console.log(`Deleting ${nodeToDel.node.data}`);
       // case 1: leaf node
       if (nodeToDel.node.leftChild === null && nodeToDel.node.rightChild === null && nodeToDel.parent !== null) {
-        console.log(`Deleting ${nodeToDel.node.data}`);
         nodeToDel.isLeftChild ? nodeToDel.parent.leftChild = null : nodeToDel.parent.rightChild = null;
         // node only has a left child
       } else if (nodeToDel.node.rightChild === null) {
         // node is the root
         if (nodeToDel.parent === null) {
-          this.updateRoot(nodeToDel.node.leftChild);
+          nodeToDel.node.leftChild ? this.updateRoot(nodeToDel.node.leftChild) : this.updateRoot(null);
         } else {
-          console.log(`Deleting ${nodeToDel.node.data}`);
+          // console.log(`Deleting ${nodeToDel.node.data}`);
           nodeToDel.isLeftChild ? nodeToDel.parent.leftChild = nodeToDel.node.leftChild : nodeToDel.parent.rightChild = nodeToDel.node.leftChild;
           // nodeToDel.parent.leftChild = nodeToDel.node.leftChild;
         }
@@ -67,52 +74,36 @@ class TreeNode {
         }
         // node has 2 children 
       } else {
-        // console.log('node has 2 children FUU');
-        //need to find successor
-        let successorNode = this.findMinNode(nodeToDel.node.rightChild, nodeToDel.node || null );
-        // console.log(`successor: ${successorNode.node.data}`);
+
+        let successorNode = this.findMinNode(nodeToDel.node.rightChild, nodeToDel.node || null);
+        console.log(`successor: ${successorNode.node.data}`);
+        console.log(`node's parent: ${nodeToDel.parent ? nodeToDel.parent.data : 'no parent'}`);
         // console.log('Node to delete!!!!!!!!');
         // console.log(nodeToDel.node);
         // console.log('Successor Node');
         // console.log(successorNode.node);
+
         // modify left child of successor, it can't have a left child by definition
         successorNode.node.leftChild = nodeToDel.node.leftChild;
-        // nodeToDel.node.leftChild = null;
-        // modify right child of successor
-        // modify connection to the parent to sever node to be deleted
+
+        //modify the right child of successor
+        if (successorNode.node.rightChild) nodeToDel.leftChild = successorNode.node.rightChild;
+        if (successorNode.node !== nodeToDel.node.rightChild) {
+          successorNode.node.rightChild = nodeToDel.node.rightChild;
+        }
+        
+        // sever succesor nodes connection to its past parent
+        successorNode.isLeftChild ? successorNode.parent.leftChild = null : successorNode.parent.rightChild = null;
         // node is the root
         if (nodeToDel.parent === null) {
-          // console.log ('Trying to delete root node!');
-          successorNode.isLeftChild ? successorNode.parent.leftChild = null : successorNode.parent.rightChild = null;
-          if (successorNode.node !== nodeToDel.node.rightChild) {
-            successorNode.node.rightChild = nodeToDel.node.rightChild;
-          }
-          if (successorNode.node.rightChild) nodeToDel.leftChild = successorNode.node.rightChild;
-
-          // console.log(`intermediate successor node`)
-          // console.log(successorNode.node);
           this.updateRoot(successorNode.node);
-
-          
-          // console.log('Final state');
-          // console.log (this);
         } else {
-          // console.log(!Object.is(successorNode.node.rightChild, nodeToDel.node.rightChild));
-          if (successorNode.node.rightChild) successorNode.parent.leftChild = successorNode.node.rightChild;
-          if ( successorNode.node !== nodeToDel.node.rightChild){
-            successorNode.node.rightChild = nodeToDel.node.rightChild;
-          }
           nodeToDel.isLeftChild ? nodeToDel.parent.leftChild = successorNode.node : nodeToDel.parent.rightChild = successorNode.node;
         }
-
-        // sever connection with parent
-
-
-        // nodeToDel.node =  null;
-
-
-        // throw new Error('Fuck you node has 2 children so you gotta wait!');
       }
+      console.log(`\nAfter Delete:`)
+      console.log(JSON.stringify(this, null, 2));
+      console.log(`END\n`)
     }
   }
 
