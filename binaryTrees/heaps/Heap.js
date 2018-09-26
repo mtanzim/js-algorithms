c5fb2889f6b8311e74e77f2aa2f1f5b16915224d
Tanzim Mokammel <mtanzim@gmail.com>
2018-09-26 18:53:55 +0800

    update heap code, fixed for empty array case

--- binaryTrees/heaps/Heap.js
+++ binaryTrees/heaps/Heap.js
@@ -2,17 +2,17 @@
 // const HeapNode = require('./heapNode');
 
 /* Notes
-
-  left child:   i * 2
-  right child:  i * 2 + 1
-  parent:       Math.floor(i / 2) 
+  Assumes 0th position is NOT NULL
+  left child:   2i+1
+  right child:  2i + 2
+  parent:       (i - 1) /  2 
 
 */
 
 // implement a max heap
 class Heap {
   constructor() {
-    // leave heap index 0 as null ? Why?
+    
     this.heapData = [];
     // this.heapData = [null];
   }
@@ -21,37 +21,73 @@ class Heap {
     console.log(this.heapData);
   }
 
+  testHeapProp () {
+    // check max val at root
+    let maxVal = this.heapData[0];
+    for (let i = 1; i < this.heapData.length; i++){
+      if (this.heapData[i] > maxVal) return false;
+    }
+    
+    // check left children are smaller
+    for (let j = 0; 2 * j + 1 <= this.heapData.length; j ++ ) {
+      // console.log (`testing left child of ${this.heapData[j]}`);
+      if (this.heapData[j] < this.heapData[2 * j + 1] ) return false;
+    }
+    
+    // check right children are smaller
+    for (let j = 0; 2 * j + 2 <= this.heapData.length; j++) {
+      // console.log(`testing right child of ${this.heapData[j]}`);
+      if (this.heapData[j] < this.heapData[2 * j + 2]) return false;
+    }
+
+    return true;
+  }
+
 
   deleteNode(input) {
     console.log(`Will delete node!`);
   }
+
+  findParentId (childId) {
+    return Math.floor((childId - 1) / 2);
+  }
   
   insertNode(input) {
     this.heapData.push(input);
-
+    console.log (`\ninserrting ${input}`)
+    console.log(`start heap: [ ${this.heapData} ]`)
+    
     // heap contains 1 or more elements
     if (this.heapData.length > 1) {
       let curIndex = this.heapData.length - 1;
-      let parentIndex = Math.floor(curIndex / 2);
+      let parentIndex = this.findParentId(curIndex);
+      
+      console.log(`current idx: ${curIndex}`);
+      console.log(`parent idx: ${parentIndex}`);
       // while the child is larger than the parent
-
+      
       while (this.heapData[curIndex] > this.heapData[parentIndex] && parentIndex >= 0) {
         // es6 destructuring: swap with parent
-        [this.heapData[parentIndex], this.heapData[curIndex]] = [this.heapData[curIndex], this.heapData[parentIndex]];
-          // go to parent index and reiterate
+        // if (this.heapData[curIndex] > this.heapData[parentIndex]){
+          console.log (`need to swap parent: ${this.heapData[parentIndex]} with new: ${this.heapData[curIndex]}`);
+          [this.heapData[parentIndex], this.heapData[curIndex]] = [this.heapData[curIndex], this.heapData[parentIndex]];
+        // }
+        // go to parent index and reiterate
         curIndex = parentIndex;
-        parentIndex = Math.floor(curIndex / 2);
+        parentIndex = this.findParentId(curIndex);
       }
-
+      
     }
+    console.log(`end heap: [ ${this.heapData} ]`)
   }
 }
 
 function driver() {
 
   let heap = new Heap();
-  [8,3,5,7,2,15,17,19].forEach (a => heap.insertNode(a));
+  [9,5,10,17,13,20,11,15,8,19].forEach (a => heap.insertNode(a));
   heap.printHeap();
+  console.log(`isHeap: ${heap.testHeapProp()}`);
 
 }
 
