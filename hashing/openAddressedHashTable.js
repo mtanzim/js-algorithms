b7158054da8b8c6d31560b0c46c8852333bcea2f
Tanzim Mokammel <mtanzim@gmail.com>
2018-10-07 20:01:24 -0400

    formatting fix

--- hashing/openAddressedHashTable.js
+++ hashing/openAddressedHashTable.js
@@ -1,27 +1,29 @@
   const hashFunc = require('./hashFunc');
   const ChainedHashTable = require('./chainedHashTable');
 
-class OAHashTable extends ChainedHashTable {
+  class OAHashTable extends ChainedHashTable {
 
-  constructor () {
-    super();
-    this.storage = [];
-    this.storageLimit = 20;
-  }
+    constructor() {
+      super();
+      this.storage = [];
+      this.storageLimit = 20;
+    }
 
-  getHash(key, probeStep) {
-    return hashFunc(key, this.storageLimit, probeStep);
-  }
+    getHash(key, probeStep) {
+      return hashFunc(key, this.storageLimit, probeStep);
+    }
 
-  //uses open addressing
-  // needs major refactor
-  add(key, value) {
+    //uses open addressing
+    // needs major refactor
+    add(key, value) {
       let probeStep = 0;
       while (probeStep < this.storageLimit) {
         let index = this.getHash(key, probeStep);
         if (this.storage[index] === undefined || this.storage[index] === 'deleted') {
           // console.log(`adding ${key} at index: ${index} with value: ${value} with probe step ${probeStep}`)
-          this.storage[index] = [[key, value]];
+          this.storage[index] = [
+            [key, value]
+          ];
           break;
         } else if (this.storage[index][0][0] === key) {
           // console.log(`replacing ${key} with value: ${value} at index: ${index}`)
@@ -30,48 +32,48 @@ class OAHashTable extends ChainedHashTable {
         } else {
           // console.log(`need to probe again :/`)
         }
-        probeStep ++;
-      }
-    // }
-  }
-
-  find(key, isDelete = false) {
-    
-    let index = this.getHash(key);
-    let found = false;
-    let probeStep = 0;
-    while (probeStep < this.storageLimit) {
-      // console.log(`probe step ${probeStep}`);
-      index = this.getHash(key, probeStep);
-
-      try {
-        if (this.storage[index][0][0] === key) {
-          // return this.storage[index][0];
-          found = true;
-          // console.log(`found key: ${key} at index: ${index}`);
-          break;
-        } 
-      } catch(err) {
-        // console.log(err);
-      } finally {
         probeStep++;
       }
+      // }
     }
 
-    if (found) {
-      if (isDelete) {
-        this.storage[index] = 'deleted';
-        // this.print();
+    find(key, isDelete = false) {
+
+      let index = this.getHash(key);
+      let found = false;
+      let probeStep = 0;
+
+      while (probeStep < this.storageLimit) {
+        // console.log(`probe step ${probeStep}`);
+        index = this.getHash(key, probeStep);
+
+        try {
+          if (this.storage[index][0][0] === key) {
+            // return this.storage[index][0];
+            found = true;
+            // console.log(`found key: ${key} at index: ${index}`);
+            break;
+          }
+        } catch (err) {
+          // console.log(err);
+        } finally {
+          probeStep++;
+        }
       }
-      return this.storage[index][0];
+
+      if (found) {
+        if (isDelete) {
+          this.storage[index] = 'deleted';
+          // this.print();
+        }
+        return this.storage[index][0];
+      }
+      throw new Error(`${key} Not Found!`);
+
     }
-    throw new Error(`${key} Not Found!`);
 
   }
 
-}
+  module.exports = OAHashTable;
 
-module.exports = OAHashTable;
-
-// driver();
-
+  // driver();
